a34c93440fb31f09209a2581cef3b504ee28843a
Tafshi Uthshow Hoque <uthshow@gmail.com>
2024-04-17 17:23:07 +0100

    Set the preferred colours for the App, this might change in the future

--- app/src/main/java/com/example/footballclub10/ui/theme/Color.kt
+++ app/src/main/java/com/example/footballclub10/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.footballclub10.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//Light Theme
+val LightBackground = Color(0XFFEAEAEB)
+val PrimaryLight = Color(0XFFF8E5E4)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//Dark Theme
+val DarkBackground = Color(0XFF333138)
+val PrimaryDark = Color(0XFFc200fb)
