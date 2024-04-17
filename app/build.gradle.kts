4f56a3317f8faef6d4d6cf135c372683c2a342c1
Tafshi Uthshow Hoque <uthshow@gmail.com>
2024-04-17 17:43:48 +0100

    Added the ROOM dependency version 2.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ android {
 
 dependencies {
 
+    val roomVersion = "2.6.1"
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -62,7 +64,10 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation ("androidx.room:room-runtime:$roomVersion")
+    implementation ("androidx.room:room-ktx:$roomVersion")
     testImplementation("junit:junit:4.13.2")
+    annotationProcessor ("androidx.room:room-compiler:$roomVersion")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
