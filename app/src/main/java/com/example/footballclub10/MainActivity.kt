0b360b528a7cce3e39fb4085e3aaada7ddc16b3b
Tafshi Uthshow Hoque <uthshow@gmail.com>
2024-04-17 17:22:22 +0100

    Display the 3 buttons when the app starts

--- app/src/main/java/com/example/footballclub10/MainActivity.kt
+++ app/src/main/java/com/example/footballclub10/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.footballclub10.ui.theme.FootBallClub10Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +14,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FootBallClub10Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    MainMenuButtons(context = this@MainActivity)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FootBallClub10Theme {
-        Greeting("Android")
     }
 }
