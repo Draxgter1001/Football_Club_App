63fa3476650e44892078ada0542b7a9e4947b516
Tafshi Uthshow Hoque <uthshow@gmail.com>
2024-04-17 17:21:23 +0100

    Gallery has been created

---
+++ app/src/main/java/com/example/footballclub10/SearchClubs.kt
@@ -0,0 +1,46 @@
+package com.example.footballclub10
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.footballclub10.ui.theme.FootBallClub10Theme
+
+class SearchClubs : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FootBallClub10Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting4("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting4(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview4() {
+    FootBallClub10Theme {
+        Greeting4("Android")
+    }
+}
