6b01b3f34b3ac297181fef51b32628ecad14fd6e
Tafshi Uthshow Hoque <uthshow@gmail.com>
2024-04-20 14:08:29 +0100

    Added a query search

--- app/src/main/java/com/example/footballclub10/data/ClubDao.kt
+++ app/src/main/java/com/example/footballclub10/data/ClubDao.kt
@@ -3,10 +3,13 @@ package com.example.footballclub10.data
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface ClubDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertClubs(clubs: List<Club>)
 
+    @Query("select * from club where strTeam like '%' || :query || '%' or strLeague like '%' || :query || '%'")
+    suspend fun searchClubs(query: String): List<Club>
 }
