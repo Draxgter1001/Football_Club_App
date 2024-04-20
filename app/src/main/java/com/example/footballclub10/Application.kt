982a62fa656da807ddf8005a47cb7b6188276023
Tafshi Uthshow Hoque <uthshow@gmail.com>
2024-04-20 14:08:12 +0100

    made the club dao initialize here since it will be used for two activities

--- app/src/main/java/com/example/footballclub10/Application.kt
+++ app/src/main/java/com/example/footballclub10/Application.kt
@@ -3,12 +3,15 @@ package com.example.footballclub10
 import android.app.Application
 import androidx.room.Room
 import com.example.footballclub10.data.AppDatabase
+import com.example.footballclub10.data.ClubDao
 
 class Application : Application() {
     override fun onCreate() {
         super.onCreate()
         db = Room.databaseBuilder(this, AppDatabase::class.java, "football_database").build()
+        club_dao = db.getClubDao()
     }
 }
 
 lateinit var db: AppDatabase
+lateinit var club_dao: ClubDao
